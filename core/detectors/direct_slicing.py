ebd685e0dcc9786b19b5fdb7277ec74d99bd4f65
MechTechnology <mechtechnology@outlook.com>
2022-02-25 23:11:10 +0200

    Adds Existing Detectors

---
+++ core/detectors/direct_slicing.py
@@ -0,0 +1,17 @@
+from typing import List
+from PIL import Image as pil
+
+class DirectSlicingDetector():
+  def run(self, combined_img: pil.Image, split_height: int, **kwargs) -> List[int]:
+    # Changes from a pil image to an numpy pixel array
+    last_row = combined_img.size[1]
+    print(last_row)
+    # Initializes some variables
+    slice_locations = [0]
+    row = split_height
+    while (row < last_row):
+      slice_locations.append(row)
+      row += split_height
+    if (slice_locations[-1] != last_row-1):
+      slice_locations.append(last_row-1)
+    return slice_locations
